780654112914ed180e92d968657cb7e2bb7df59a
danielvs01 <danielvsolano1@gmail.com>
2025-02-11 18:14:29 -0600

    Cambios a casi todo

--- js/script.js
+++ js/script.js
@@ -107,3 +107,77 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const botones = document.querySelectorAll(".filter-btn");
+    const productos = document.querySelectorAll(".producto");
+
+    botones.forEach(boton => {
+        boton.addEventListener("click", () => {
+            const categoria = boton.getAttribute("data-category");
+
+            productos.forEach(producto => {
+                if (categoria === "todos" || producto.getAttribute("data-category") === categoria) {
+                    producto.style.display = "block";
+                } else {
+                    producto.style.display = "none";
+                }
+            });
+        });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.querySelector("form");
+    const emailInput = document.getElementById("email");
+
+    form.addEventListener("submit", function(event) {
+        const emailValue = emailInput.value.trim();
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if (!emailRegex.test(emailValue)) {
+            event.preventDefault();
+            alert("Por favor, ingrese un correo electrónico válido.");
+            emailInput.focus();
+        }
+    });
+});
+
+//Script para la validacion de los campos del formulario
+document.addEventListener('DOMContentLoaded', function() {
+    const details = document.querySelectorAll('.faq details');
+
+    details.forEach(detail => {
+        detail.addEventListener('toggle', function() {
+            if (this.open) {
+                details.forEach(otherDetail => {
+                    if (otherDetail !== this) {
+                        otherDetail.open = false;
+                    }
+                });
+            }
+        });
+    });
+});
+
+//Script para el faq
+document.addEventListener("DOMContentLoaded", function() {
+    const faqQuestions = document.querySelectorAll(".faq-question");
+
+    faqQuestions.forEach((question) => {
+        question.addEventListener("click", function() {
+            const answer = question.nextElementSibling;
+            const isOpen = answer.style.display === "block";
+            
+            // Cerrar todas las respuestas
+            document.querySelectorAll(".faq-answer").forEach((answer) => {
+                answer.style.display = "none";
+            });
+            
+            // Abrir la respuesta seleccionada si no estaba abierta
+            if (!isOpen) {
+                answer.style.display = "block";
+            }
+        });
+    });
+});
